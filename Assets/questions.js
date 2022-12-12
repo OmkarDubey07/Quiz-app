94333c7e3992f291f468a0694c524a5e0fdb0ca9
omkar nath dubey <omkardubey1530@gmail.com>
2022-12-12 18:47:08 +0530

    html , css completet

---
+++ Assets/questions.js
@@ -0,0 +1,51 @@
+export const questions =[
+    {
+        'question' : 'Which of the following is a markup language ?',
+        'a':'HTML',
+        'b':'CSS',
+        'c':'Javascript',
+        'd':'PHP',
+        'correct':'a'
+    },
+    {
+        'question' : 'What does CSS stands for ?',
+        'a':'Hypertext Markup Language',
+        'b':'Cascading Style Sheet',
+        'c':'Jason Object Notation',
+        'd':'Senior Citizen Savings Scheme',
+        'correct':'b'
+    },
+    {
+        'question' : 'What year was JavaScript Launched ?',
+        'a':'1996',
+        'b':'1995',
+        'c':'1994',
+        'd':'none of above',
+        'correct':'b'
+    },
+    {
+        'question' : 'Which of the following methods can be used to display data in some form using Javascript?',
+        'a':'Document.write()',
+        'b':'Console.log()',
+        'c':'Window.alert()',
+        'd':'All of above',
+        'correct':'d'
+    },
+    {
+        'question' : 'How can a datatype be declared to be a constant type?',
+        'a':'constant',
+        'b':'var',
+        'c':'const',
+        'd':'let',
+        'correct':'c'
+    },
+    {
+        'question' : 'Which of the following methods is used to access HTML elements using Javascript?',
+        'a':'getElementById()',
+        'b':'getElementByClassName()',
+        'c':'Both A and B',
+        'd':'None of the above',
+        'correct':'c'
+    }
+];
+
